992e68cc636d0b4571d177ad41ef8354b63f6787
peterpickles <lipeter2006@gmail.com>
2018-06-08 23:00:03 -0700

    adding more psudocode for highlights

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,13 +6,15 @@ class About extends Component {
       <section id="about-page">
         <h1 className="about-page-header">About</h1>
         <div className="about-headshot-wrapper">
-            <img className="about-headshot" src={require("../.././media/images/headshot.jpg")} alt="Peter Li's headshot"/>
-            <ul className="about-name-wrapper">
-                <li>Peter Li</li>
-                <li>Full Stack Developer</li>
-            </ul>
+          <img className="about-headshot" src={require("../.././media/images/headshot.jpg")} alt="Peter Li's headshot" />
+          <ul className="about-name-wrapper">
+            <li>Peter Li</li>
+            <li>Full Stack Developer</li>
+          </ul>
         </div>
-        <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia atque consequuntur omnis quaerat delectus a amet. In, illum nostrum placeat impedit reiciendis iste, quia aperiam eius, officia odio ab recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nulla molestiae reiciendis sit obcaecati sequi id illum nam quos asperiores hic, error, enim molestias inventore accusantium tempora architecto veniam repellendus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, aperiam! Maiores exercitationem quam facilis incidunt explicabo? Nam officia vitae velit aspernatur dolor similique cupiditate, dolorum tempore provident sunt itaque sequi.</p>
+        <p className="about-text">
+          text about me
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia atque consequuntur omnis quaerat delectus a amet. In, illum nostrum placeat impedit reiciendis iste, quia aperiam eius, officia odio ab recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nulla molestiae reiciendis sit obcaecati sequi id illum nam quos asperiores hic, error, enim molestias inventore accusantium tempora architecto veniam repellendus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, aperiam! Maiores exercitationem quam facilis incidunt explicabo? Nam officia vitae velit aspernatur dolor similique cupiditate, dolorum tempore provident sunt itaque sequi.</p>
       </section>
     );
   }
